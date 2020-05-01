c95c9ab5eb52191f3de2732d609369a1182260d0
mintch2001 <64130149+mintch2001@users.noreply.github.com>
2020-05-01 16:55:43 +0700

    Add files via upload

---
+++ SQL1/SQL1/DataAccess.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SQL1
+{
+    class DataAccess
+    {
+      public static void CreateTable ()
+        {
+            using (SqliteConnection db = new SqliteConnection("Filename=customers.db")) //เชื่อมต่อกับไฟล์ DataBase
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT " + //สร้างตารางขึ้นมาหากยังไม่มี
+                                      "EXISTS customers (Number INTEGER," + //ตารางชื่อ customers โดนในตารางจะมีฟิลด์ที่ชื่อ Number เเละ Name
+                                      "Name NVACHAR(50) NULL)";
+
+                SqliteCommand sqliteCommand = new SqliteCommand(tableCommand, db);
+                sqliteCommand.ExecuteReader();
+            }
+        }
+        public static void AddData(string inputText)
+        {
+            using (SqliteConnection db = new SqliteConnection("Filename=customers.db")) //เชื่อมต่อกับไฟล์ DataBase
+            {
+                db.Open();
+
+                SqliteCommand sqliteCommand = new SqliteCommand();
+                sqliteCommand.Connection = db; //เชื่อมค่อ Command เข้ากับไฟล์ DataBase ที่เราตั้งไว้
+
+                sqliteCommand.CommandText = "INSERT INTO customers VALUES (1, @Name)"; //ทำการใส่ข้อมูลลงไปในตารางที่ชื่อว่า customers
+                sqliteCommand.Parameters.AddWithValue("@Name", inputText); //ไว้สำหรับรับข้อมูลที่ผู้ใช้ต้องกรอก
+                //เติม @ เพื่อไม่ให้คำสั่ง SQL ที่ผู้ใช้กรอกเข้ามาทำงานเพื่อป้องกันผู้ไม่หวังดี
+
+                sqliteCommand.ExecuteReader();
+
+                db.Close();
+            }
+        }
+        public static List<string> ShowDetail()
+        {
+            List<string> data = new List<string>();
+
+            using (SqliteConnection db = new SqliteConnection("Filename=customers.db"))
+            {
+                db.Open();
+
+                SqliteCommand Name = new SqliteCommand("SELECT Name from customers",db); //ทำการหยิบข้อมูลที่อยู่ในช่อง Name ออกมา
+                SqliteCommand Number = new SqliteCommand("SELECT Number from customers", db); //ทำการหยิบข้อมูลที่อยู่ในช่อง Number ออกมา
+
+                //ทำการแปลงข้อมูลที่หยิบออกมาให้สามารถนำไปใช้ต่อได้
+                SqliteDataReader nameData = Name.ExecuteReader(); 
+                SqliteDataReader NumberData = Number.ExecuteReader();
+
+                // Read() จะทำการส่งค่่า True กลับมาในกรณีที่ยังมีเเถวเหลืออยู่ และจะกลายเป็น False เมื่อบนนณทัดนั้นว่าง
+                while (nameData.Read() && NumberData.Read())
+                {
+                    data.Add(NumberData.GetString(0) + " " + nameData.GetString(0));
+                }
+                db.Close();
+            }
+            return data;
+        }
+    
+    }
+}
---
+++ SQL1/SQL1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQL1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> a;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataAccess.CreateTable();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            a = new List<string>();
+
+            string set = " ";
+            foreach (string i in DataAccess.ShowDetail())
+            {
+                set = set + "\n" +i;
+            }
+            MessageBox.Show("Name Account : \n" + set);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(txtInput.Text);
+        }
+    }
+}
